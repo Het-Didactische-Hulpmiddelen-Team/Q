ba917b588d039f7a6bb0251b5db7d48381ec0ae6
BelgianNoise <king.arthur360@gmail.com>
2020-02-11 17:15:53 +0100

    toevoegen in Q

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const queue = document.querySelector('#students');
 $('#logout-button').css('display', 'none');
+$('#enterQ-button').css('display', 'none');
+let name = "";
 
 db.collection('queue').orderBy('datetime').onSnapshot(snapshot => {
   let changes = snapshot.docChanges();
@@ -35,23 +37,45 @@ function login(){
     let firstname = splittedemail[0];
     let lastname = splittedemail[1].split("@")[0];
     let span = document.createElement('span');
+    name = firstname + " " + lastname;
 
     // show logged in user
-    span.textContent = `Logged in as ${firstname} ${lastname}`;
+    span.textContent = `Logged in as ${name}`;
     user.innerHTML = "";
     user.appendChild(span); 
 
     // close modal
     $("#modal-login").modal("toggle");
 
-    // show logout button
+    // show logout button and enterQ button
     $('#logout-button').css('display', 'inline-block');
+    $('#enterQ-button').css('display', 'inline-block');
     // hide login button
     $('#login-button').css('display', 'none');
   }
 }
 function logout(){
   $('#logout-button').css('display', 'none');
+  $('#enterQ-button').css('display', 'none');
   $('#login-button').css('display', 'inline-block');
   document.querySelector('#user').innerHTML = "";
+}
+
+function enterQ(){
+// TO DO CHECK IF ALREADY IN Q
+// SHOW / MAKE LEAVE Q BUTTON
+
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+  var h = today.getHours();
+  var min = today.getMinutes();
+  var s = today.getSeconds();
+  d = yyyy + '-' + mm + '-' + dd + " " + h+":"+min+":"+s;
+
+  db.collection('queue').add({
+    name: name,
+    datetime: d
+});
 }
