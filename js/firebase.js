d4e5b476f51a95e81d3c9df5374ca00b18902b89
arnevandoorslaer <arne.vandoorslaer@student.ucll.be>
2020-02-11 15:33:30 +0100

    queue kan weergegeven worden, pseudo login scherm

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAIb5RAb0Q-x5xWao_y1bg9Tfy2wFX9O5Q",
+  authDomain: "queue-8e855.firebaseapp.com",
+  databaseURL: "https://queue-8e855.firebaseio.com",
+  projectId: "queue-8e855",
+  storageBucket: "queue-8e855.appspot.com",
+  messagingSenderId: "560085374241",
+  appId: "1:560085374241:web:db837e02a1ab1133e79a60",
+  measurementId: "G-ZXSY2VJZZ9"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
---
+++ js/main.js
@@ -0,0 +1,40 @@
+const queue = document.querySelector('#students');
+
+db.collection('queue').orderBy('datetime').onSnapshot(snapshot => {
+  let changes = snapshot.docChanges();
+  changes.forEach(change => {
+      if(change.type == 'added'){
+          displayName(change.doc);
+      } else if (change.type == 'removed'){
+          let li = queue.querySelector('[data-id=' + change.doc.id + ']');
+          queue.removeChild(li);
+      }
+  });
+});
+
+function displayName(doc) {
+  let li = document.createElement('li');
+  let span = document.createElement('span');
+
+  li.setAttribute('data-id', doc.id); 
+  li.className = 'list-group-item text-center';
+  span.textContent = doc.data().name;
+
+  li.appendChild(span);
+  queue.appendChild(li);
+}
+
+function login(){
+  let email = document.querySelector('#email').value;
+  let user = document.querySelector('#user');
+  let splittedemail = email.split(".");
+  let firstname = splittedemail[0];
+  let lastname = splittedemail[1].split("@")[0];
+  let span = document.createElement('span');
+  
+  span.textContent = `Logged in as ${firstname} ${lastname}`
+  user.appendChild(span);
+
+  console.log(firstname);
+  console.log(lastname);
+}
