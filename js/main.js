be2f484e934fb4b4aeac590e4f1ebb3ba6410f94
BelgianNoise <king.arthur360@gmail.com>
2020-02-11 16:47:23 +0100

    inloggen

--- js/main.js
+++ js/main.js
@@ -25,16 +25,32 @@ function displayName(doc) {
 }
 
 function login(){
-  let email = document.querySelector('#email').value;
-  let user = document.querySelector('#user');
-  let splittedemail = email.split(".");
-  let firstname = splittedemail[0];
-  let lastname = splittedemail[1].split("@")[0];
-  let span = document.createElement('span');
+  let email = document.querySelector('#email').value.trim();
+  let regex = new RegExp('\\w{3,}\\.\\w{3,}@(student\\.)?ucll\\.be');
+  if(regex.test(email)){
+    // get firstname and name from email
+    let user = document.querySelector('#user');
+    let splittedemail = email.split(".");
+    let firstname = splittedemail[0];
+    let lastname = splittedemail[1].split("@")[0];
+    let span = document.createElement('span');
 
-  span.textContent = `Logged in as ${firstname} ${lastname}`;
-  user.innerHTML = span;
+    // show logged in user
+    span.textContent = `Logged in as ${firstname} ${lastname}`;
+    user.innerHTML = "";
+    user.appendChild(span); 
 
-  console.log(firstname);
-  console.log(lastname);
+    // close modal
+    $("#modal-login").modal("toggle");
+
+    // show logout button
+    $('#logout-button').css('display', 'inline-block');
+    // hide login button
+    $('#login-button').css('display', 'none');
+  }
+}
+function logout(){
+  $('#logout-button').css('display', 'none');
+  $('#login-button').css('display', 'inline-block');
+  $('#user').innerHTML = "";
 }
